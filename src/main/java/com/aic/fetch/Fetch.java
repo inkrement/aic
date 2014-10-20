f69326494805e272c754280b8608875b5de1c2e5
Florian Taus <florian.taus@hotmail.com>
2014-10-21 00:43:47 +0200

    Java file for fetching, for credentials see Telegram

---
+++ src/main/java/com/aic/fetch/Fetch.java
@@ -0,0 +1,95 @@
+package com.aic.fetch;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+/**
+ * The class for fetching tweets.
+ * 
+ * @author Florian Taus
+ */
+public class Fetch
+{
+	private final static SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
+	private final static String CONFIGURATION_FILE = "twitter.properties";
+
+	/**
+	 * Fetches (at most 100) tweets for the given name, filtered by the
+	 * {@code start} and {@code end} date.
+	 * <p>
+	 * <b>Note:</b>
+	 * "The Search API is not complete index of all Tweets, but instead an index of recent Tweets. At the moment that index includes between 6-9 days of Tweets."
+	 * (see {@link https://dev.twitter.com/rest/public/search})
+	 * </p>
+	 * 
+	 * @param name
+	 *            The term the tweets should conatin
+	 * @param start
+	 *            The start date for the search
+	 * @param end
+	 *            The end date for the search
+	 * @return A list of tweets matching the criterias (at most 100)
+	 */
+	public List<String> fetch(String name, Date start, Date end)
+	{
+		List<String> tweetList = new ArrayList<String>();
+
+		try
+		{
+			twitter4j.Twitter twitter = new twitter4j.TwitterFactory(getConfiguration()).getInstance();
+
+			twitter4j.Query query = new twitter4j.Query(name);
+
+			query.setSince(dateFormat.format(start));
+			query.setUntil(dateFormat.format(end));
+			query.setCount(100);
+
+			twitter4j.QueryResult result;
+			result = twitter.search(query);
+
+			List<twitter4j.Status> tweets = result.getTweets();
+			for (twitter4j.Status tweet: tweets)
+			{
+				tweetList.add(tweet.getText());
+			}
+		}
+		catch (twitter4j.TwitterException e)
+		{
+			e.printStackTrace();
+			System.err.println("Failed to search tweets: " + e.getMessage());
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			System.err.println("Failed to open configuration: " + e.getMessage());
+		}
+
+		return tweetList;
+	}
+
+	private twitter4j.conf.Configuration getConfiguration() throws IOException
+	{
+		twitter4j.conf.ConfigurationBuilder cb = new twitter4j.conf.ConfigurationBuilder();
+		cb.setDebugEnabled(true);
+		Properties props = new Properties();
+		props.load(new FileInputStream(CONFIGURATION_FILE));
+		if (props.containsKey("consumerKey") && props.containsKey("consumerSecret") && props.containsKey("accessToken") && props.containsKey("accessTokenSecret"))
+		{
+			cb.setOAuthConsumerKey(props.getProperty("consumerKey"));
+			cb.setOAuthConsumerSecret(props.getProperty("consumerSecret"));
+			cb.setOAuthAccessToken(props.getProperty("accessToken"));
+			cb.setOAuthAccessTokenSecret(props.getProperty("accessTokenSecret"));
+			return cb.build();
+		}
+		else
+		{
+			throw new IOException("Credentials incomplete!");
+		}
+	}
+}
---
+++ src/main/java/com/aic/fetch/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The package for implementing the functionality to fetch tweets.
+ * @author Florian Taus
+ *
+ */
+package com.aic.fetch;
