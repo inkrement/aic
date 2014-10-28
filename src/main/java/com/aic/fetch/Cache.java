969d33738b5c3c1df806cfb6169494ccc3d515b5
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-10-28 21:37:00 +0100

    clear cache and get file paths added

--- src/main/java/com/aic/fetch/Cache.java
+++ src/main/java/com/aic/fetch/Cache.java
@@ -5,19 +5,28 @@ import java.util.List;
 import java.io.*;
 import com.aic.components.*;
 
+import java.nio.file.*;
+
 /**
  * @todo implement
  */
 
 public class Cache{
 	//TODO: will not work on windows
-	private static final String path = "/tmp";
+	public static final File path = new File("/tmp/aic");
 
 	/**
 	 * loads a list of already cached datasets
 	 * cuts overlapping parts that are not in the given timerange
 	 */
 	public static List<TwitterStatusList> load(String name, Date start, Date end){
+
+		//find all files matching name_*
+
+
+		//parse timestamps and eventually load files
+
+
 		return null;
 	}
 
@@ -26,6 +35,10 @@ public class Cache{
 	 */
 	public static void store(TwitterStatusList tweets){
 		String filename = path + "/" + tweets.getKeyword() + "_" + tweets.getStart().getTime() + "_" + tweets.getEnd().getTime();
+		//TODO: create subfolder
+
+		path.getParentFile().mkdirs();
+
 
 		try{
 			FileOutputStream f_out = new FileOutputStream(filename);
@@ -34,12 +47,41 @@ public class Cache{
 			// Write object out to disk
 			obj_out.writeObject ( tweets );
 		} catch(FileNotFoundException e){
-			System.err.println("could not find file: " + filename);
+			System.err.println("could not find file: " + filename + " " + e.getMessage());
 		} catch(IOException e){
-			System.err.println("could not write to file: " + filename);
+			System.err.println("could not write to file: " + filename + " " + e.getMessage());
 		}
 
 	}
 
+
+	public static File[] getFilePaths(final String keyword){
+		File dir = new File(path.getPath());
+		File [] files = dir.listFiles(new FilenameFilter() {
+		    @Override
+		    public boolean accept(File dir, String name) {
+		        return name.startsWith(keyword);
+		    }
+		});
+
+		return files;
+	}
+
+	public static void clear(){
+		clearDirectory(path);
+	}
+
+	public static void clearDirectory(File dir){
+		//Files.deleteIfExists(path);
+		for(File file: dir.listFiles()) file.delete();
+	}
+
+/*
+	private void writeToFile(File file, String content){
+		File file = new File("C:\\user\\Desktop\\dir1\\dir2\\filename.txt");
+		file.getParentFile().mkdirs();
+		FileWriter writer = new FileWriter(file);
+	}
+*/
 
 }
--- src/test/java/com/aic/fetch/CacheTest.java
+++ src/test/java/com/aic/fetch/CacheTest.java
@@ -9,10 +9,13 @@ import com.aic.fetch.Cache;
 import java.io.File;
 import java.util.Date;
 
+import java.io.IOException;
+
 public class CacheTest{
 
 	@Test
 	public void fileCreated() {
+		Cache.clear();
 
 		TwitterStatusList tweetlist = new TwitterStatusList("microsoft");
 		Date timestamp = new Date();
@@ -20,7 +23,51 @@ public class CacheTest{
 
 		Cache.store(tweetlist);
 
-		File file = new File("/tmp/" + "microsoft_" + timestamp.getTime() + "_" + timestamp.getTime());
+		File file = new File(Cache.path + "/microsoft_" + timestamp.getTime() + "_" + timestamp.getTime());
 		assertTrue(file.exists());
+
+		Cache.clear();
+	}
+
+
+	@Test
+	public void findAllKeywordFilePaths(){
+		String keyword = "microsoft";
+
+		Cache.clear();
+
+		Date timestamp = new Date();
+		TwitterStatusList tweetlist = new TwitterStatusList(keyword);
+		tweetlist.add(new TwitterStatus(timestamp, "inkrement", "some #fancy tweet @twitter"));
+
+		Cache.store(tweetlist);
+
+		File[] files = Cache.getFilePaths(keyword);
+		
+		assertEquals(files.length, 1);
+		//System.out.println("files found: " + files[0].toString());
+		//System.out.println( "should be: " + Cache.path + "/" + keyword + "_" + timestamp.getTime() + "_" + timestamp.getTime() );
+		assertEquals(files[0].toString(), Cache.path + "/" + keyword + "_" + timestamp.getTime() + "_" + timestamp.getTime() );
+
+		Cache.clear();
+	}
+
+	@Test
+	public void clearCache() {
+		String filepath = Cache.path.getPath() + "/somerandomfile";
+		System.out.println("filepath: " + filepath);
+
+		File f = new File(filepath);
+		f.mkdirs();
+		try{
+			f.createNewFile();
+		} catch (IOException e){
+			fail("this assertion should never be thrown");
+		}
+
+		Cache.clear();
+
+		File file = new File(filepath);
+		assertFalse(file.exists());
 	}
 }
