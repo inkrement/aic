04339ed811133053920ed3b83ec9fe5e32de5ca8
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-11-01 13:10:43 +0100

    parser example added

--- src/test/java/com/aic/ExampleTest.java
+++ src/test/java/com/aic/ExampleTest.java
@@ -1,8 +1,13 @@
 package com.aic;
 
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import edu.stanford.nlp.parser.shiftreduce.ShiftReduceParser;
+import edu.stanford.nlp.process.DocumentPreprocessor;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreePrint;
 import org.junit.Test;
 
 import edu.stanford.nlp.ling.*;
@@ -10,6 +15,7 @@ import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.util.*;
 
 
+import java.io.StringReader;
 import java.util.List;
 import java.util.Properties;
 
@@ -33,7 +39,31 @@ public class ExampleTest {
 
             Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
             int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
-            System.out.println("Sentiment: " + sentiment + " String: " + sentence.toString());
+            //System.out.println("Sentiment: " + sentiment + " String: " + sentence.toString());
+        }
+    }
+
+
+
+    @Test
+    public void justAsimpleParserDemo() {
+        final String PCG_MODEL = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
+        //String modelPath = "edu/stanford/nlp/models/srparser/englishSR.ser.gz";
+
+        String taggerPath = "edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger";
+
+        String text = "My dog likes to shake his stuffed chickadee toy.";
+
+        MaxentTagger tagger = new MaxentTagger(taggerPath);
+        //ShiftReduceParser model = ShiftReduceParser.loadModel(modelPath);
+        LexicalizedParser parser = LexicalizedParser.loadModel(PCG_MODEL);
+
+        DocumentPreprocessor tokenizer = new DocumentPreprocessor(new StringReader(text));
+        for (List<HasWord> sentence : tokenizer) {
+            List<TaggedWord> tagged = tagger.tagSentence(sentence);
+            Tree tree = parser.apply(tagged);
+            //System.out.println(tree);
+            new TreePrint("penn,typedDependenciesCollapsed").printTree(tree);
         }
     }
 
