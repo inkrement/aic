b819900784eacc5b97193655de33618db5981673
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2014-11-23 12:12:31 +0100

    Add normalization in preprocessing

--- src/main/java/com/aic/preprocessing/SentimentTwitterPreprocessor.java
+++ src/main/java/com/aic/preprocessing/SentimentTwitterPreprocessor.java
@@ -8,21 +8,52 @@ import edu.stanford.nlp.process.PTBTokenizer;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SentimentTwitterPreprocessor implements ISentimentPreprocessor {
+
+    private static final String URL_PATTERN = "((https?|ftp|gopher|telnet|file|Unsure|http):" +
+            "((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
 
     @Override
     public FeatureVector preprocess(String message) throws PreprocessingException {
         PTBTokenizer.PTBTokenizerFactory<CoreLabel> fac = PTBTokenizer.PTBTokenizerFactory.newPTBTokenizerFactory(true, true);
         List<Feature> features = new ArrayList<Feature>();
+
         for (CoreLabel label : fac.getTokenizer(new StringReader(message)).tokenize()) {
-            Feature feature = new Feature();
-            feature.setWord(label.word());
-            features.add(feature);
+            String word = normalizeWord(label.word());
+            if (!word.isEmpty()) {
+                Feature feature = new Feature();
+                feature.setWord(word);
+                features.add(feature);
+            }
         }
         FeatureVector featureVector = new FeatureVector();
         featureVector.setFeatures(features);
 
         return featureVector;
+    }
+
+    public String normalizeWord(String word) {
+        // lowercase word
+        word = word.toLowerCase();
+        // remove URL
+        word = removeUrl(word);
+
+        return word;
+    }
+
+    private String removeUrl(String word) {
+        Pattern p = Pattern.compile(URL_PATTERN, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(word);
+        int i = 0;
+
+        while (m.find()) {
+            word = word.replaceAll(m.group(i), "").trim();
+            i++;
+        }
+
+        return word;
     }
 }
--- src/test/java/com/aic/preprocessing/TweetPreprocessTest.java
+++ src/test/java/com/aic/preprocessing/TweetPreprocessTest.java
@@ -21,14 +21,36 @@ public class TweetPreprocessTest {
      * tokens in a given message.
      */
 	@Test
-	public void tokensCorrectlyParsed() throws PreprocessingException {
+	public void testIftokensCorrectlyParsed() throws PreprocessingException {
         FeatureVector featureVector = preprocessor.preprocess("5 7/8 U.K. na you're ur http://orf.at @stum450n Hi. Thank u for you the follow. Nice website..." +
                 " check your carriage returns though, the bottom of 'Website &amp; lol brand identity' is out (unhappy)");
         List<String> features = new ArrayList<String>();
         for (Feature f : featureVector.getFeatures()) {
             features.add(f.getWord());
         }
-        Assert.assertTrue("'Nice' not found",  features.contains("Nice"));
+        Assert.assertTrue("'nice' not found",  features.contains("nice"));
         Assert.assertFalse("'happy' found but not contained in message",  features.contains("happy"));
 	}
+
+    /**
+     * Tests if the preprocessor finds the normalized word tokens
+     * within a given message.
+     */
+    @Test
+    public void testIfWordsNormalized() throws PreprocessingException {
+        FeatureVector featureVector = preprocessor.preprocess("Make use of LOWERCASE and remove" +
+                        " URLs like http://wikipedia.org, https://wikipedia.org and http://www.wikipedia.org");
+        List<String> features = new ArrayList<String>();
+        for (Feature f : featureVector.getFeatures()) {
+            features.add(f.getWord());
+        }
+        Assert.assertTrue("'make' not found",  features.contains("make"));
+        Assert.assertFalse("'Make' found but not contained in message",  features.contains("Make"));
+        Assert.assertTrue("'lowercase' not found",  features.contains("lowercase"));
+        Assert.assertTrue("'urls' not found",  features.contains("urls"));
+        Assert.assertFalse("'URLs' not found",  features.contains("URLs"));
+        Assert.assertFalse("'http://wikipedia.org' not found",  features.contains("http://wikipedia.org"));
+        Assert.assertFalse("'https://wikipedia.org' not found",  features.contains("https://wikipedia.org"));
+        Assert.assertFalse("'http://www.wikipedia.org' not found",  features.contains("http://www.wikipedia.org"));
+    }
 }
