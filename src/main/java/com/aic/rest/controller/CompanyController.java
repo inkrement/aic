6e349254d097ecd6b5cd77e74e88d850ca010928
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2014-11-23 22:53:49 +0100

    Refactor controller

--- src/main/java/com/aic/rest/controller/CompanyController.java
+++ src/main/java/com/aic/rest/controller/CompanyController.java
@@ -1,36 +1,50 @@
-package com.aic.rest;
+package com.aic.rest.controller;
 
-import com.aic.classification.ClassificationException;
-import com.aic.classification.SentimentClassifier;
-import com.aic.fetch.*;
-import com.aic.components.*;
-
+import com.aic.rest.RestException;
+import com.aic.rest.domain.Company;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
+import java.util.Hashtable;
 import java.util.List;
-import java.util.ArrayList;
 
 /**
  * Handles the REST requests that are sent to the server.
  */
-@RestController
-public class Controller {
+@Controller
+@RequestMapping("/")
+public class CompanyController {
+
+    private Hashtable<String, Company> companies;
+
+    public CompanyController() {
+        this.companies = new Hashtable<>();
+    }
+
+    @RequestMapping(value = "", method=RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
 
     /**
      * Register a company within the system.
+     *
+     * TODO: throw exception if company already exists.
      *
      * @param name
      * @param password
      */
     @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ResponseBody
     public void register(@RequestParam(value = "name") String name,
                          @RequestParam(value = "password") String password) {
-
+        Company company = new Company(name, password);
+        companies.put(name, company);
     }
 
     /**
@@ -42,43 +56,31 @@ public class Controller {
      * @param end
      */
     @RequestMapping(value = "/sentiment", method = RequestMethod.GET)
+    @ResponseBody
     public Double sentiment(@RequestParam(value = "name") String name,
-                               @RequestParam(value = "password") String password,
-                               @RequestParam(value = "start", required = false)
-                                    @DateTimeFormat(pattern = "MMddyyyy") Date start,
-                               @RequestParam(value = "end", required = false)
-                                    @DateTimeFormat(pattern = "MMddyyyy") Date end) throws ClassificationException {
-
-        //fetch tweets
-        List<TwitterStatus> l = Fetch.get(name, start, end);
-
-	    // TODO pass valid training data
-        SentimentClassifier c = new SentimentClassifier(null);
-
-        int pos = 0;
-
-        for(TwitterStatus ts: l){
-            /*if(c.classify(ts.getContent()).equals(com.aic.classification.Sentiment.POSITIVE))
-                pos++;*/
+                            @RequestParam(value = "password") String password,
+                            @RequestParam(value = "start", required = false)
+                            @DateTimeFormat(pattern = "MMddyyyy") Date start,
+                            @RequestParam(value = "end", required = false)
+                            @DateTimeFormat(pattern = "MMddyyyy") Date end) throws RestException {
+        if (!companies.containsKey(name)) {
+            throw new RestException("Company not registered!");
+        }
+        if (!companies.get(name).getPassword().equals(password)) {
+            throw new RestException("Wrong password!");
         }
 
-        return ((double)pos)/l.size();
+        return 0.0;
     }
     
     @RequestMapping(value = "/tweets", method = RequestMethod.GET)
+    @ResponseBody
     public List<String> tweets(@RequestParam(value = "name") String name,
                                @RequestParam(value = "start", required = false)
                                     @DateTimeFormat(pattern = "yyyyMMdd") Date start,
                                @RequestParam(value = "end", required = false)
                                     @DateTimeFormat(pattern = "yyyyMMdd") Date end) {
-       Fetch f = new Fetch();
 
-       List<String> tweets = new ArrayList<String>();
-
-       for(TwitterStatus t: f.get(name, start, end)){
-            tweets.add(t.toString());
-       }
-
-       return tweets;
+        return null;
     }
 }
